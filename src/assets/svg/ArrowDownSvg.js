152704825e49f0832a9bd16a0978c816ce3022d4
“Enrique <efranco@joonik.com>
2022-03-10 15:15:02 -0400

    feat: add new assets

---
+++ src/assets/svg/ArrowDownSvg.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ArrowDownSvg = () => {
+  return (
+    <svg
+      viewBox='0 0 24 24'
+      fill='none'
+    >
+      <path
+        d='M19 8.5L12 15.5L5 8.5'
+        stroke='currentColor'
+        strokeWidth='1.5'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  )
+}
+
+export default ArrowDownSvg
